b1e0a446f402f20dee6770209a904c17cf0f9d11
Jazmin M <jazminmerckel@gmail.com>
2019-09-18 02:30:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,8 +16,10 @@ def words_five_letters_long(text)
   text.scan(/\b[a-z]{5}\b/)
 end
 
+#use A-Z for any capital letter
+#
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match() ? true : false
+  text.match([A-Z]) ? true : false
 end
 
 def valid_phone_number?(phone)
